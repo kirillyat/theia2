f1e0ff94b7689e9cc7cefab9dbbe8f9aac749124
Кирилл Яценко <kirillyat@gmail.com>
2020-10-28 12:07:37 +0300

    Add files via upload

---
+++ catcoding/out/extension.js
@@ -0,0 +1,62 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const cats = {
+    'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+    'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif'
+};
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "catcoding" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('catcoding.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from CatCoding!');
+    });
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('catcoding.start', () => {
+        // Create and show a new webview
+        const panel = vscode.window.createWebviewPanel('catCoding', // Identifies the type of the webview. Used internally
+        'Cat Coding', // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+        );
+        let iteration = 0;
+        const updateWebview = () => {
+            const cat = iteration++ % 2 ? 'Compiling Cat' : 'Coding Cat';
+            panel.title = cat;
+            panel.webview.html = getWebviewContent(cat);
+        };
+        // Set initial content
+        updateWebview();
+        // And schedule updates to the content every 2 second
+        setInterval(updateWebview, 2000);
+    }));
+}
+exports.activate = activate;
+function getWebviewContent(cat) {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <img src="${cats[cat]}" width="300" />
+  </body>
+  </html>`;
+}
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
